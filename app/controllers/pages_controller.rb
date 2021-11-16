79c2a3d4f622f7a78831215e50049b8d9576bae9
Teresa Finsterwald <teresa.finsterwald@gmail.com>
2021-11-16 17:24:30 +0100

    fixed conflicts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
-<<<<<<< HEAD
-  skip_before_action :authenticate_user!, only: [:home, :index, :show]
-=======
   # skip_before_action :authenticate_user!, only: [:home, :index, :show]
->>>>>>> master
 
   def home
   end
