63b5cdbc3bafd1ee8e2aa03af9d38f102eded12e
Valerie Finsterwald <finsterwald.valerie@gmail.com>
2021-11-16 16:42:22 +0100

    Create Feature for bokking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,23 @@ class BookingsController < ApplicationController
     @desk = Desk.find(params[:desk_id])
     @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(params_booking)
+    @desk = Desk.find(params[:desk_id])
+    @booking.desk = @desk
+    @booking.user = current_user
+    @booking.save
+    if @booking.save
+      redirect_to desks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_booking
+    params.require(:booking).permit(:booked_on)
+  end
 end
