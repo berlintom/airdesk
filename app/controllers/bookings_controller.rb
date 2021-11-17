8494f8ad238eb8f17a682ac17241e984e17a2aaa
Teresa Finsterwald <teresa.finsterwald@gmail.com>
2021-11-17 15:43:13 +0100

    cardstyle

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -21,7 +21,7 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.save
     if @booking.save
-      redirect_to desks_path
+      redirect_to bookings_path
     else
       render :new
     end
